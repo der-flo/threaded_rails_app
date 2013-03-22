5a882462669a11e9aa97879d0c143b852286a087
Florian Dütsch <florian.duetsch@nix-wie-weg.de>
2013-03-22 17:18:08 +0100

    Generated empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Threaded::Application.config.secret_token = '813c8896dfecbee71782c8355b94c5d90cd0f75f020465ce31b5b7e665253be4d0ebbece5784ff53d2e2a708764809d52b5f7a1ff0be090bb8244a95fd2ae575'
